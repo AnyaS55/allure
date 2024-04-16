127da47a4e183d95e99930a7f7e0095dbbe5003c
Anya <annasedova123456@gmail.com>
2024-04-16 22:32:18 +0300

    change test

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -59,7 +59,7 @@ class DeliveryTest {
                 .shouldBe(visible);
         $("[data-test-id='replan-notification'] .button").click();
         $("[data-test-id='success-notification'] .notification__content")
-                .shouldHave(exactText("Встреча Прекрасно запланирована на " + secondMeetingDate))
+                .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate))
                 .shouldBe(visible);
     }
 }
